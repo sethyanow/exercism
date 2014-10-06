0b670862d839252e0639f98b64452db3b47be66d
Seth Yanow <seth.yanow@gmail.com>
2014-10-06 00:16:44 -0400

    refactored

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,40 +1,24 @@
-# * `G` -> `C`
-# * `C` -> `G`
-# * `T` -> `A`
-# * `A` -> `U`
-
-
 class Complement
 
-  def self.flip_dna(strand)
-    case strand
-      when 'G' then 'C'
-      when 'C' then 'G'
-      when 'T' then 'A'
-      when 'A' then 'U'
-    end
-  end
+  DNA_SEQUENCE = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+  RNA_SEQUENCE = DNA_SEQUENCE.invert
 
   def self.of_dna(dna)
-    return flip_dna(dna) if dna.length == 1
+    return DNA_SEQUENCE[dna] if dna.length == 1
     dna.chars.map { |strand|
-      flip_dna(strand)
+      DNA_SEQUENCE[strand]
     }.join()
   end
 
-  def self.flip_rna(strand)
-    case strand
-      when 'G' then 'C'
-      when 'C' then 'G'
-      when 'A' then 'T'
-      when 'U' then 'A'
-    end
-  end
-
   def self.of_rna(rna)
-    return flip_rna(rna) if rna.length == 1
+    return RNA_SEQUENCE[rna] if rna.length == 1
     rna.chars.map { |strand|
-      flip_rna(strand)
+      RNA_SEQUENCE[strand]
     }.join()
   end
 
