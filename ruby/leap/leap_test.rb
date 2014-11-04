c8c24994dcc8ee5ded019a8e0b5f5965fd79f7c0
Seth Yanow <seth.yanow@gmail.com>
2014-11-04 16:46:05 -0500

    leap

--- ruby/leap/leap_test.rb
+++ ruby/leap/leap_test.rb
@@ -17,22 +17,22 @@ class YearTest < MiniTest::Unit::TestCase
   end
 
   def test_non_leap_year
-    skip
+    
     refute Year.leap?(1997)
   end
 
   def test_non_leap_even_year
-    skip
+    
     refute Year.leap?(1998)
   end
 
   def test_century
-    skip
+    
     refute Year.leap?(1900)
   end
 
   def test_fourth_century
-    skip
+    
     assert Year.leap?(2400)
   end
 end
