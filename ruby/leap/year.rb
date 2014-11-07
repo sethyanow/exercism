3add409d58d0c3f3302277e123249aea1eb7ce94
Seth Yanow <seth.yanow@gmail.com>
2014-11-07 11:33:13 -0500

    refactoring

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -2,11 +2,13 @@ module Year
   extend self
 
   def leap?(year)
-    return false if century(year)
+    return false if invalid?(year)
     year % 4 == 0
   end
 
-  def century(year)
+  private
+
+  def invalid?(year)
     year % 100 == 0 && year % 400 != 0
   end
 end
